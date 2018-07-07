6923902c4235aae2e62188a6bca74c6315ddd2c1
justindachille <justindachille@gmail.com>
2018-07-07 16:47:32 -0700

    started creating router

--- prototype1/src/app/app.module.ts
+++ prototype1/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+
+const appRoutes: Routes = [
+	{ path: 'about', component: AboutComponent},
+	{ path: 'home', component: HomeComponent},
+	{ path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+		RouterModule.forRoot(appRoutes,
+			{ enableTracing : true}
+		)
+  ],
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    HomeComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
