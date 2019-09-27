687219bc1f7ae3351cb2ab12b53c2a5e742f0be2
justindachille <justindachille@gmail.com>
2019-09-27 14:54:00 -0700

    Auto-generated commit

---
+++ app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { resizeHeader } from '../assets/js/header.js'; 
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'app';
+	
+	constructor(private router : Router) {
+	}
+
+	onAnchorClick() {
+		console.log("angular");
+		resizeHeader("onAnchorClick");
+	}
+	
+	onActivate(event) {
+		resizeHeader("onActivate");
+	}
+}
+
